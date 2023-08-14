431d5ffe3f6e4f87f3f465c07775b900e0e98a63
suojae <suojae3@gmail.com>
2023-08-14 14:26:41 +0900

    Initial Commit

---
+++ SnsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnsProject
+//
+//  Created by ㅣ on 2023/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
